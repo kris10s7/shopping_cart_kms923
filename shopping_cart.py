787b66640272452bc86f84262f5e8ee8230d53a6
kris10s7 <50898383+kris10s7@users.noreply.github.com>
2019-06-15 22:06:41 -0400

    Currency Formatting

--- shopping_cart.py
+++ shopping_cart.py
@@ -50,12 +50,16 @@ while True:
 # INFO DISPLAY
 
 #print(selected ids)
+    
 for selected_id in selected_ids:
         matching_products = [p for p in products if str(p[("id")]) == str(selected_id)]
         matching_product =  matching_products[0]
         total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+    ## format price in USD
+        total_price_usd = "${0:.2f}".format(total_price)
+        price_usd = "${0:.2f}".format(matching_product["price"])
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(price_usd))
 
-print("TOTAL PRICE: " + str(total_price))
+print("TOTAL PRICE: " + str(total_price_usd))
 
 
